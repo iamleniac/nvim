872f1911be6a112bfa08ef43bb3b00637f1f568d
Lenilson Nascimento <lenilson.nascimentolima@gi-de.com>
2026-01-28 10:43:40 +0100

    add oilgit

--- lua/leniac/plugins/oil.lua
+++ lua/leniac/plugins/oil.lua
@@ -1,24 +1,15 @@
 return {
-
   'stevearc/oil.nvim',
-
   dependencies = {
-
     'echasnovski/mini.icons',
   },
-
   config = function()
     require('oil').setup {
-
       default_file_explorer = true,
-
       view_options = {
-
         show_hidden = true,
       },
-
       columns = {
-
         {
 
           'icon',
