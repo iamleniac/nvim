59848d095f2c2553bb2afe19f1dfb6f2516d6888
Lenilson Nascimento <lenilson.nascimentolima@gi-de.com>
2026-01-28 14:53:29 +0100

    add peek commands

--- lua/plugins/peek.lua
+++ lua/plugins/peek.lua
@@ -6,5 +6,8 @@ return {
     require('peek').setup {
       app = 'browser',
     }
+
+    vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+    vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
   end,
 }
